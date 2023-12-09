31b371169fb3e2d35593b14ff811a10863273c7e
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-12-09 20:04:45 +0800

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+        int counter = 0, n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (Math.abs(nums[i] - nums[j]) == k) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+}
